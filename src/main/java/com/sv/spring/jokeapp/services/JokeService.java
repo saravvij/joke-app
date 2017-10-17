497cd1b3f4f64980b87f3fb79ec2945ab1f0680b
Saravanan Vijayappan <sarav@saravanans-mbp.lan>
2017-10-17 11:06:05 -0700

    init

---
+++ src/main/java/com/sv/spring/jokeapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.sv.spring.jokeapp.services;
+
+public interface JokeService {
+
+    public String getJoke();
+}
---
+++ src/main/java/com/sv/spring/jokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.sv.spring.jokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return this.chuckNorrisQuotes.getRandomQuote();
+    }
+}
